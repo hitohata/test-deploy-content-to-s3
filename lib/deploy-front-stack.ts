1788b220d8b6430c28a4d403f237e420a1a14df9
hirohata <hirohatatro@gmail.com>
2024-08-20 10:24:07 -0700

    add

--- lib/deploy-front-stack.ts
+++ lib/deploy-front-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudFrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DeployFrontStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const contentBucket = new s3.Bucket(this, "ContentBucket", {
+      bucketName: "content-bucket-bucket-bucket",
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DeployFrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucketOrigin = new cloudFrontOrigins.S3Origin(contentBucket);
+
+    new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin:  bucketOrigin,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD
+      }
+    })
+
   }
 }
